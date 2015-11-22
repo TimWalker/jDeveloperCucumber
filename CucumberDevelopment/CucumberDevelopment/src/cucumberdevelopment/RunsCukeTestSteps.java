5cde4c284d6a5e5adfbc0fd71ea6d40fea1ddece
Tim Walker <walketim@gmail.com>
2015-11-22 14:28:53 -0600

    first commit

---
+++ CucumberDevelopment/CucumberDevelopment/src/cucumberdevelopment/RunsCukeTestSteps.java
@@ -0,0 +1,27 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.PendingException;
+import cucumber.deps.com.thoughtworks.xstream.io.path.Path;
+
+public class RunsCukeTestSteps {
+
+@Given("^I have some foos to fight$")
+public void i_have_some_foos_to_fight() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I fight foos$")
+public void i_fight_foos() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^I should have fought foos$")
+public void i_should_have_fought_foos() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+}
---
+++ CucumberDevelopment/CucumberDevelopment/src/features/foo.feature
@@ -0,0 +1,10 @@
+Feature: foo
+In order to foo
+As a foo
+I'd like to foo
+
+Scenario: foo fight
+	Given I have some foos to fight
+	When I fight foos
+	Then I should have fought foos
+        
